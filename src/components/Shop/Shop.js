c060f288aa16ca29b0d8e700de0818edcac32b2d
AkasHZamaN <akashzaman4@gmail.com>
2022-03-26 23:34:04 +0600

    question

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -42,6 +42,16 @@ const Shop = () => {
             <div className="cart-container">
                 <Cart foodCart = {foodCart} removeItems={removeItems} selectOneItem={selectOneItem} ></Cart>
             </div>
+            <div>
+                <div className='question'>
+                <p>Q1. State এবং Props এর মধ্যে পার্থক্য কি ?</p>
+                <small><span>উত্তর:</span> State অভ্যন্তরীণ এবং element দ্বারা নিয়ন্ত্রিত হয় যখন প্রপগুলি বাহ্যিক হয় এবং উপাদানটি রেন্ডার করে যা কিছু দ্বারা নিয়ন্ত্রিত হয়। State এবং Props উভয়ই একটি উপাদানের মধ্যে ডেটা নিয়ন্ত্রণ করতে ব্যবহৃত হয়, সাধারণত Props parents দ্বারা সেট করা হয় এবং child elements এ প্রেরণ করা হয় এবং সেগুলি সমগ্র element জুড়ে স্থির করা হয়।</small>
+                </div>
+                <div className='question-2'>
+                    <p>Q2 State কিভাবে কাজ করে ?</p>
+                    <small><span>উত্তর:</span> ইভেন্ট হ্যান্ডলার, সার্ভার প্রতিক্রিয়া, বা প্রপ পরিবর্তনের প্রতিক্রিয়ায় State আপডেট করা যায় । এটি setState() পদ্ধতি ব্যবহার করে করা হয়। setState() পদ্ধতিটি কম্পোনেন্ট স্টেটে করা সমস্ত আপডেটকে সারিবদ্ধ করে এবং কম্পোনেন্ট এবং এর চাইল্ড আপডেট করা অবস্থায় পুনরায় রেন্ডার করার জন্য প্রতিক্রিয়াকে নির্দেশ করে।</small>
+                </div>
+            </div>
         </div>
     );
 };
